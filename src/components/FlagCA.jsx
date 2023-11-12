9a5483394df2e3186a0d36215d10d204106ab7e4
Denis Anfruns <anfruns.denis@gmail.com>
2023-11-12 01:27:11 +0100

    Add styles to language dropdown button

---
+++ src/components/FlagCA.jsx
@@ -0,0 +1,14 @@
+// FlagOfCatalonia.jsx
+import React from 'react';
+
+const FlagOfCatalonia = () => (
+  <svg class='inline mr-1' xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 36 36'>
+    <path id="svg_1" d="m36,27a4,4 0 0 1 -4,4l-28,0a4,4 0 0 1 -4,-4l0,-18a4,4 0 0 1 4,-4l28,0a4,4 0 0 1 4,4l0,18z" fill="#FFC400"/>
+    <path id="svg_2" d="m0,8.43227l36,0l0,2.7482l-36,0l0,-2.7482z" fill="#C60A1D"/>
+    <path id="svg_16" d="m0,13.83767l36,0l0,2.7482l-36,0l0,-2.7482z" fill="#C60A1D"/>
+    <path id="svg_18" d="m0,19.24308l36,0l0,2.7482l-36,0l0,-2.7482z" fill="#C60A1D"/>
+    <path id="svg_20" d="m0,24.64848l36,0l0,2.7482l-36,0l0,-2.7482z" fill="#C60A1D"/>
+  </svg>
+);
+
+export default FlagOfCatalonia;
--- src/components/LanguageDropdown.jsx
+++ src/components/LanguageDropdown.jsx
@@ -1,69 +1,90 @@
-import React, { useEffect, useState } from "react";
-import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
+import React, { useEffect, useState } from 'react'
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from '@nextui-org/react'
+import FlagES from './FlagES.jsx';
+import FlagCA from './FlagCA.jsx';
+import FlagUK from './FlagUK.jsx';
 
-export default function App() {
-  const [language, setLanguage] = React.useState('');
+export default function LanguageDropdown() {
+	const [language, setLanguage] = useState('')
+	const [Flag, setFlag] = useState(null);
 
-  useEffect(() => {
-    const path = window.location.pathname;
-    const pathSegments = path.split('/').filter(Boolean);
-    const languageSegment = pathSegments[0];
+	useEffect(() => {
+		const path = window.location.pathname
+		const pathSegments = path.split('/').filter(Boolean)
+		const languageSegment = pathSegments[0]
 
-    let URLLanguage;
-    switch (languageSegment) {
-      case 'es-es':
-        URLLanguage = 'Español';
-        break;
-      case 'ca-es':
-        URLLanguage = 'Català';
-        break;
-      default:
-        URLLanguage = 'English';
-        break;
-    }
+		let URLLanguage
+		switch (languageSegment) {
+			case 'es-es':
+				setFlag(<FlagES />);
+				URLLanguage = 'Español';
+				break
+			case 'ca-es':
+				setFlag(<FlagCA />);
+				URLLanguage = 'Català';
+				break
+			default:
+				setFlag(<FlagUK />);
+				URLLanguage = 'English';
+				break
+		}
 
-    setLanguage(URLLanguage);
-  }, []);
+		setLanguage(URLLanguage)
+	}, [])
 
+	function handleLanguageSelection(keys) {
+		const key = Array.from(keys)[0]
+		let newPath
+		switch (key) {
+			case 'Español':
+				newPath = '/es-es'
+				break
+			case 'Català':
+				newPath = '/ca-es'
+				break
+			default:
+				newPath = '/en'
+				break
+		}
+		window.location.assign(newPath)
+	}
 
-  function handleLanguageSelection(keys) {
-    const key = Array.from(keys)[0];
-    let newPath;
-    switch (key) {
-      case 'Español':
-        newPath = '/es-es';
-        break;
-      case 'Català':
-        newPath = '/ca-es';
-        break;
-      default:
-        newPath = '/en';
-        break;
-    }
-    window.location.assign(newPath); 
-  }
-
-  return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button 
-          variant="bordered">
-          {language}
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu 
-        aria-label="Language dropdown button selector"
-        variant="flat"
-        closeOnSelect={true}
-        disallowEmptySelection
-        selectionMode="single"
-        selectedKeys={language}
-        onSelectionChange={handleLanguageSelection}
-      >
-        <DropdownItem key="Español">🇪🇸 &nbsp;Español</DropdownItem>
-        <DropdownItem key="Català">Català</DropdownItem>
-        <DropdownItem key="English">🇬🇧 &nbsp;English</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  );
+	return (
+		<Dropdown
+			classNames={{
+				content: "bg-default-100",
+			}}>
+			<DropdownTrigger>
+				<Button variant='bordered'>{Flag} {language}</Button> 
+			</DropdownTrigger>
+			<DropdownMenu
+			  itemClasses={{
+          base: [
+            "white",
+            "transition-opacity",
+            "data-[hover=true]:text-foreground",
+            "data-[hover=true]:bg-default-300",
+            "dark:data-[hover=true]:bg-default-300",
+            "data-[focus-visible=true]:ring-default-500",
+          ],
+        }}
+				aria-label='Language dropdown button selector'
+				variant='flat'
+				closeOnSelect={true}
+				disallowEmptySelection
+				selectionMode='single'
+				selectedKeys={language}
+				onSelectionChange={handleLanguageSelection}>
+				<DropdownItem key='Español'>
+					<FlagES /> &nbsp;Español
+				</DropdownItem>
+				<DropdownItem key='Català'>
+					<FlagCA/>	&nbsp;Català
+				</DropdownItem>
+				<DropdownItem key='English'>
+					<FlagUK /> &nbsp;English
+				</DropdownItem>
+			</DropdownMenu>
+		</Dropdown>
+	)
 }
