782479444e4a5f489ba89272e3cd344f904ae196
HunkStalker <daanfruns@gmail.com>
2024-04-10 21:16:57 +0200

    Fix hybrid SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  output: "hybrid",
+  adapter: vercel(),
 });
