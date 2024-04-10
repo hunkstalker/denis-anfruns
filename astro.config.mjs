33fe9c031878e60d7f508a41e27ef6f9805d98a8
HunkStalker <daanfruns@gmail.com>
2024-04-10 21:26:42 +0200

    Maybe SSR Hybrid fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel()
 });
