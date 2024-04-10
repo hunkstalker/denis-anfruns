4d91e1d07483ca88e0165483653ce240d58f931b
HunkStalker <daanfruns@gmail.com>
2024-04-10 02:37:16 +0200

    Add hybrid SSR to Author.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "hybrid",
+  adapter: vercel()
 });
