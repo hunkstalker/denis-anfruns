ea1ff4f1f23cbff72b95fa3f04f024fc7d64009c
HunkStalker <daanfruns@gmail.com>
2024-06-07 03:22:11 +0200

    Cambios de estilo y texto en portada

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,17 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
+  },
+	theme: {
     container: {
       center: true,
     },
