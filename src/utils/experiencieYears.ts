ba6e49f1f921ec75705f91f51109785f53031ba2
HunkStalker <daanfruns@gmail.com>
2025-09-12 18:37:09 +0200

    Eliminado cálculo innecesario de fecha

--- src/utils/experiencieYears.ts
+++ src/utils/experiencieYears.ts
@@ -1,6 +1,6 @@
 export default function experiencieYears(): number {
   const startWorkingDual: number = 1;
-  const startWorkingYear: number = new Date('2022-09-01').getFullYear();
+  const startWorkingYear: number = 2022;
   const currentYear: number = new Date().getFullYear();
 
   return Math.abs(currentYear - startWorkingYear) + startWorkingDual;
