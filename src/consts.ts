8f89a6389dc1eeb8202739281d1c8773d194d2b3
Denis Anfruns <daanfruns@gmail.com>
2025-11-26 14:42:41 +0100

    Arregla footer, espaciados y estructura

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,14 @@
+import { SiAstro, SiReact, SiDotnet } from "react-icons/si";
+import { FaMicrosoft, FaGlobe } from "react-icons/fa";
+
 export const SKILLS = [
-  "Power Apps",
-  "Power Automate",
-  "Astro",
-  "React",
-  "SharePoint",
-  ".NET MAUI",
-  "APIs Web nativas"
+  { name: "Power Apps", icon: FaMicrosoft },
+  { name: "Power Automate", icon: FaMicrosoft },
+  { name: "Astro", icon: SiAstro },
+  { name: "React", icon: SiReact },
+  { name: "SharePoint", icon: FaMicrosoft },
+  { name: ".NET MAUI", icon: SiDotnet },
+  { name: "APIs Web nativas", icon: FaGlobe }
 ];
 
 export const SITE_TITLE = 'Denis A. M. Portfolio';
