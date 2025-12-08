68aec113d08c43e8eeb9f1d197cfe0d1d573b22d
Denis Anfruns <daanfruns@gmail.com>
2025-12-08 05:58:31 +0100

    Refactoriza los botones de navegación

---
+++ src/i18n/navigation.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+  { key: 'nav.home', path: '/', enabled: true },
+  { key: 'nav.blog', path: '/blog', enabled: true },
+  { key: 'nav.projects', path: '/projects', enabled: false },
+] as const
