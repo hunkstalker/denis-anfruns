e037b184f7b464948aacd33a1626154ba161944b
Denis Anfruns <daanfruns@gmail.com>
2025-12-11 09:59:08 +0100

    Fix FOUC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import inline from '@playform/inline';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://denis-anfruns.dev',
-  integrations: [tailwind(), react(), inline(), mdx()],
+  integrations: [tailwind(), react(), mdx()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'ca'],
