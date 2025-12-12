b01892a3beeea04ed8c32e7c9c603032da332913
Denis Anfruns <daanfruns@gmail.com>
2025-12-12 01:22:02 +0100

    Integra el componente SearchBadges

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./packages/astro-search-badges/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+	],
 	theme: {
     fontSize: {
       sm: '0.8rem',
