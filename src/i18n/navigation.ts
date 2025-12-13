8d8801024b5c8e0349b48e333d7f1e1993287b76
Denis Anfruns <daanfruns@gmail.com>
2025-12-13 02:21:25 +0100

    Añade sección About

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -2,4 +2,5 @@ export const navItems = [
   { key: 'nav.home', path: '/', enabled: true },
   { key: 'nav.blog', path: '/blog', enabled: true },
   { key: 'nav.projects', path: '/projects', enabled: false },
+  { key: 'nav.about', path: '/about', enabled: true },
 ] as const
