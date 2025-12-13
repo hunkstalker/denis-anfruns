91a8586bf4838fc5e0e6abb2a8e1c95bb850aa6a
Denis Anfruns <daanfruns@gmail.com>
2025-12-13 17:58:40 +0100

    Fix navegación

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { key: 'nav.home', path: '/', enabled: true },
-  { key: 'nav.blog', path: '/devlog', enabled: true },
-  { key: 'nav.projects', path: '/projects', enabled: false },
-  { key: 'nav.about', path: '/about', enabled: true },
+  { key: 'nav.blog', path: '/devlog/', enabled: true },
+  { key: 'nav.projects', path: '/projects/', enabled: false },
+  { key: 'nav.about', path: '/about/', enabled: true },
 ] as const
